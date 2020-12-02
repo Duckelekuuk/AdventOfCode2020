bc0fa231741f10c80fb700bc32f570be6ede6315
Duckelekuuk <duckelekuuk@gmail.com>
2020-12-02 16:43:21 +0100

    Improved solution day 2

--- day-2/src/main/java/com/duckelekuuk/adventofcode/DayTwoPartTwo.java
+++ day-2/src/main/java/com/duckelekuuk/adventofcode/DayTwoPartTwo.java
@@ -5,10 +5,13 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class DayTwoPartTwo {
+
+    private static final Pattern INPUT_PATTERN = Pattern.compile("([^>]+)-([^>]+) ([^>]+): ([^>]+)");
 
     public static void main(String[] args) {
         Stream<String> input = getInput("input.txt");
@@ -23,20 +26,16 @@ public class DayTwoPartTwo {
     }
 
     private static boolean validatePassword(String input) {
-        String[] split = input.split(": ");
-        String pattern = split[0];
-        String password = split[1];
+        Matcher matcher = INPUT_PATTERN.matcher(input);
 
-        String[] patternSplit = pattern.split(" ");
+        if (!matcher.find()) return false;
 
-        int firstIndex = Integer.parseInt(patternSplit[0].split("-")[0]) - 1;
-        int secondIndex = Integer.parseInt(patternSplit[0].split("-")[1]) - 1;
-        char character = patternSplit[1].charAt(0);
+        final int firstIndex = Integer.parseInt(matcher.group(1)) - 1;
+        final int secondIndex = Integer.parseInt(matcher.group(2)) - 1;
+        final char character = matcher.group(3).charAt(0);
+        final String password = matcher.group(4);
 
-        boolean result = password.charAt(firstIndex) == character ^ password.charAt(secondIndex) == character;
-
-        System.out.println(character + " - " + password.charAt(firstIndex) + ":" + password.charAt(secondIndex) + " = " + result);
-        return result;
+        return password.charAt(firstIndex) == character ^ password.charAt(secondIndex) == character;
     }
 
     public static Stream<String> getInput(String resourceName) {
